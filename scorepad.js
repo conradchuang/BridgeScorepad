6d73fa3739f0b83a9eb9891b2c810c582bb5e9c0
Conrad Huang <conradchuang@gmail.com>
2024-04-11 14:49:31 -0700

    Remove player name fields now that they can be edited by clicking the seat in the table view Reorder to group related functions together

--- scorepad.js
+++ scorepad.js
@@ -43,6 +43,7 @@ OtherSide = {
     ew: "ns",
     ns: "ew",
 }
+NameConnector = " &ndash; ";
 
 TotalScore = { ns:0, ew:0 };
 HandResults = [];
@@ -56,26 +57,14 @@ REContractDetails = /([1234567])([NSHDC])(X{0,2})([NESW])/;
 /* Result is of form "[+-]#" */
 REResult = /([-+]?)(\d+)/;
 
-function set_vulnerability(side, onoff) {
-    let players = document.getElementsByClassName(side);
-    for (let player of players) {
-        if (onoff)
-            player.classList.add("vulnerable");
-        else
-            player.classList.remove("vulnerable");
-    }
-    Vulnerability[side] = onoff;
-}
+/*
+ * Event handlers
+ */
 
-function get_player_seat(id) {
-    return id.split("-")[1];
-}
-
-function update_name(ev) {
-    /* this = element triggering event */
-    let name = this.value || "";
-    let seat = get_player_seat(this.id);
-    document.getElementById(seat + "-name").innerHTML = name;
+function change_system(ev) {
+    confirm_show("Switch to " + this.value + " scoring?<br/>" +
+                 "All current and total scores will be erased.",
+                 set_system.bind(this, this.value), null);
 }
 
 function edit_name(ev) {
@@ -88,6 +77,59 @@ function update_dealer(ev) {
     /* this = element triggering event */
     show_dealer(this.getAttribute("seat"));
 }
+
+function update_contract(ev) {
+    if (ev.keyCode != 13)
+        return;
+    ev.preventDefault();
+    let contract_info = parse_contract(this.value);
+    if (contract_info == null) {
+        alert_show("That is not a valid contract.", focus_on_contract);
+        return;
+    }
+    this.value = contract_info.whole;
+    /* Display final contract on board */
+    let seat = SeatAbbr[contract_info.declarer];
+    for (let s in NextSeat) {
+        let ce = document.getElementById(s + "-contract");
+        ce.innerHTML = s == seat ? html_contract(contract_info, false) : "";
+    }
+    /* Disable contract input and move focus to result input field */
+    let result = document.getElementById("input-result");
+    result.value = "";
+    result.disabled = false;
+    result.focus();
+}
+
+function input_undo(ev) {
+    if (HandResults.length == 0)
+        alert_show("There are no results to undo", null);
+    else
+        confirm_show("Remove last contract score?", undo, null);
+}
+
+function input_clear(ev) {
+    confirm_show("Clear all scores, including totals?", clear_all, null);
+}
+
+/*
+ * Vulnerability functions
+ */
+
+function set_vulnerability(side, onoff) {
+    let players = document.getElementsByClassName(side);
+    for (let player of players) {
+        if (onoff)
+            player.classList.add("vulnerable");
+        else
+            player.classList.remove("vulnerable");
+    }
+    Vulnerability[side] = onoff;
+}
+
+/*
+ * Dealer functions
+ */
 
 function show_dealer(seat) {
     if (seat != null) {
@@ -135,28 +177,9 @@ function show_dealer(seat) {
     }
 }
 
-function update_contract(ev) {
-    if (ev.keyCode != 13)
-        return;
-    ev.preventDefault();
-    let contract_details = parse_contract_details(this.value);
-    if (contract_details == null) {
-        alert_show("That is not a valid contract.", focus_on_contract);
-        return;
-    }
-    this.value = contract_details.whole;
-    /* Display final contract on board */
-    let seat = SeatAbbr[contract_details.declarer];
-    for (let s in NextSeat) {
-        let ce = document.getElementById(s + "-contract");
-        ce.innerHTML = s == seat ? html_contract(contract_details, false) : "";
-    }
-    /* Disable contract input and move focus to result input field */
-    let result = document.getElementById("input-result");
-    result.value = "";
-    result.disabled = false;
-    result.focus();
-}
+/*
+ * Scorepad functions
+ */
 
 function clear_section(id) {
     document.getElementById(id).innerHTML = "";
@@ -173,45 +196,6 @@ function clear_scorepad() {
     set_vulnerability("ew", false);
     BelowRows = [];
     NextRow = { ns: 0, ew: 0 };
-}
-
-function parse_contract_details(s) {
-    let ns = s.replace(/\s/g, '').toUpperCase();
-    let parts = ns.match(REContractDetails);
-    if (parts == null)
-        return null;
-    return { whole: parts[0],
-             level: parts[1],
-             suit: parts[2],
-             doubled: parts[3],
-             declarer: parts[4] }
-}
-
-function parse_result(s) {
-    let ns = s.replace(/\s/g, '').toUpperCase();
-    /* Contract is of form (level suit [double] [redouble] seat) */
-    let parts = ns.match(REResult);
-    if (parts == null)
-        return null;
-    if (parts[1] == "")
-        parts[1] = "+";
-    return { whole: parts[0],
-             sign: parts[1],
-             value: parts[2] }
-}
-
-function html_contract(contract_details, include_declarer) {
-    let s = [contract_details.level,
-             SuitName[contract_details.suit],
-             contract_details.doubled];
-    if (include_declarer)
-        s.push(contract_details.declarer);
-    return s.join("");
-}
-
-function contract_label(deal_index, contract_details, result_info) {
-    return (deal_index+1) + ": " + html_contract(contract_details, true) +
-           result_info.sign + result_info.value;
 }
 
 function make_side_row(side) {
@@ -293,26 +277,12 @@ function undo_above_score(state) {
     state.row.remove();
 }
 
-function part_scores() {
-    let part_scores = {ns:0, ew:0};
-    for (let result of HandResults) {
-        part_scores[result.winning_side] += result.score_below;
-        if (part_scores[result.winning_side] >= 100)
-            part_scores = {ns:0, ew:0};
-    }
-    return part_scores;
-}
-
-function part_score(side) {
-    return part_scores()[side];
-}
-
-function update_scorepad(contract_details, result) {
-    let level = parseInt(contract_details.level);
-    let suit = contract_details.suit;
+function update_scorepad(contract_info, result) {
+    let level = parseInt(contract_info.level);
+    let suit = contract_info.suit;
     /* 0:undoubled 1:doubled 2:redoubled */
-    let doubled = contract_details.doubled.length;
-    let seat = contract_details.declarer;
+    let doubled = contract_info.doubled.length;
+    let seat = contract_info.declarer;
     let side = seat.match(/[EW]/) ? "ew" : "ns";
     let tricks = parseInt(result.value);
     if (result.sign == "-")
@@ -321,7 +291,7 @@ function update_scorepad(contract_details, result) {
         throw new RangeError("too few tricks for contract");
     let vulnerable = Vulnerability[side]
     let deal_index = HandResults.length;
-    let label = contract_label(deal_index, contract_details, result)
+    let label = contract_label(deal_index, contract_info, result)
     let desc = level + " " + SuitName[suit];
     if (doubled == 1)
         desc += " doubled";
@@ -337,7 +307,7 @@ function update_scorepad(contract_details, result) {
     }
     /* Add result to history */
     let hand_result = {deal_index: deal_index,
-                       contract_details: contract_details,
+                       contract_info: contract_info,
                        result: result};
     /* Check if declaring side is vulnerable */
     /* Compute score and potential bonus */
@@ -561,6 +531,15 @@ function show_match_totals() {
     document.getElementById("score-ew-match").innerHTML = match_total.ew;
 }
 
+function show_accumulated_totals() {
+    document.getElementById("score-ns-total").innerHTML = TotalScore.ns;
+    document.getElementById("score-ew-total").innerHTML = TotalScore.ew;
+}
+
+/*
+ * End of match functions
+ */
+
 function end_match(end_of_match) {
     let final = {ns:0, ew:0};
     let msg;
@@ -578,6 +557,74 @@ function end_match(end_of_match) {
     eom_show(msg);
 }
 
+function eom_show(msg) {
+    show_accumulated_totals();
+    document.getElementById("eom-text").innerHTML = msg;
+    document.getElementById("eom-dialog").show();
+}
+
+function eom_close(ev) {
+    ev.preventDefault();
+    document.getElementById("eom-dialog").close();
+    document.getElementById("input-contract").focus();
+    clear_scorepad();
+    next_hand();
+}
+
+/*
+ * Contract functions
+ */
+
+function parse_contract(s) {
+    let ns = s.replace(/\s/g, '').toUpperCase();
+    let parts = ns.match(REContractDetails);
+    if (parts == null)
+        return null;
+    return { whole: parts[0],
+             level: parts[1],
+             suit: parts[2],
+             doubled: parts[3],
+             declarer: parts[4] }
+}
+
+function html_contract(contract, include_declarer) {
+    let s = [contract.level,
+             SuitName[contract.suit],
+             contract.doubled];
+    if (include_declarer)
+        s.push(contract.declarer);
+    return s.join("");
+}
+
+function contract_label(deal_index, contract_info, result_info) {
+    return (deal_index+1) + ": " + html_contract(contract_info, true) +
+           result_info.sign + result_info.value;
+}
+
+function focus_on_contract() {
+    document.getElementById("input-contract").focus();
+}
+
+/*
+ * Result functions
+ */
+function parse_result(s) {
+    let ns = s.replace(/\s/g, '').toUpperCase();
+    /* Contract is of form (level suit [double] [redouble] seat) */
+    let parts = ns.match(REResult);
+    if (parts == null)
+        return null;
+    if (parts[1] == "")
+        parts[1] = "+";
+    return { whole: parts[0],
+             sign: parts[1],
+             value: parts[2] }
+}
+
+function html_result(result_info) {
+    return result_info.sign + result_info.value;
+}
+
 function update_result(ev) {
     if (ev.keyCode != 13)
         return;
@@ -590,7 +637,7 @@ function update_result(ev) {
     /* Update score pad */
     let ce = document.getElementById("input-contract");
     try {
-        update_scorepad(parse_contract_details(ce.value), result_info);
+        update_scorepad(parse_contract(ce.value), result_info);
     } catch (e) {
         if (e instanceof RangeError) {
             alert_show(e.message, focus_on_result);
@@ -622,12 +669,13 @@ function next_hand() {
     result.disabled = true;
 }
 
-function input_undo() {
-    if (HandResults.length == 0)
-        alert_show("There are no results to undo", null);
-    else
-        confirm_show("Remove last contract score?", undo, null);
+function focus_on_result() {
+    document.getElementById("input-result").focus();
 }
+
+/*
+ * Undo functions
+ */
 
 function undo() {
     /* Remove last result */
@@ -649,6 +697,7 @@ function undo() {
             }
         }
     }
+    show_match_totals();
     /* Show dealer for last hand */
     let dealers = document.getElementsByClassName("dealing");
     if (dealers.length != 1) {
@@ -661,9 +710,9 @@ function undo() {
     show_dealer(prev_seat);
 }
 
-function input_clear() {
-    onfirm_show("Clear all scores, including totals?", clear_all, null);
-}
+/*
+ * Clear scores functions
+ */
 
 function clear_all() {
     clear_scorepad();
@@ -673,12 +722,57 @@ function clear_all() {
     show_dealer(null);
 }
 
-function change_system() {
-    confirm_show("Switch to " + this.value + " scoring?<br/>" +
-                 "All current and total scores will be erased.",
-                 set_system.bind(this, this.value), null);
+/*
+ * Edit name functions
+ */
+
+function edit_name_show(seat) {
+    let name = document.getElementById(seat + "-name").innerHTML;
+    if (name.startsWith(NameConnector))
+        name = name.slice(NameConnector.length);
+    document.getElementById("edit-name-input").value = name;
+    let d = document.getElementById("edit-name-dialog");
+    d.seat = seat;
+    d.showModal();
 }
 
+function edit_name_finished(ev) {
+    if (ev.keyCode != 13)
+        return;
+    ev.preventDefault();
+    edit_name_close(ev);
+}
+
+function edit_name_close(ev) {
+    let d = document.getElementById("edit-name-dialog");
+    d.close();
+    let name = document.getElementById("edit-name-input").value;
+    if (name)
+        name = NameConnector + name;
+    document.getElementById(d.seat + "-name").innerHTML = name;
+    d.seat = null;
+}
+
+/*
+ * Part score functions
+ */
+function part_scores() {
+    let part_scores = {ns:0, ew:0};
+    for (let result of HandResults) {
+        part_scores[result.winning_side] += result.score_below;
+        if (part_scores[result.winning_side] >= 100)
+            part_scores = {ns:0, ew:0};
+    }
+    return part_scores;
+}
+
+function part_score(side) {
+    return part_scores()[side];
+}
+
+/*
+ * Change score system functions
+ */
 function set_system(system) {
     let above = document.getElementById("above-the-line");
     let below = document.getElementById("below-the-line");
@@ -699,18 +793,9 @@ function set_system(system) {
     ScoreSystem = system;
 }
 
-function show_accumulated_totals() {
-    document.getElementById("score-ns-total").innerHTML = TotalScore.ns;
-    document.getElementById("score-ew-total").innerHTML = TotalScore.ew;
-}
-
-function focus_on_contract() {
-    document.getElementById("input-contract").focus();
-}
-
-function focus_on_result() {
-    document.getElementById("input-result").focus();
-}
+/*
+ * Utility functions
+ */
 
 function alert_show(msg, cb) {
     document.getElementById("alert-text").innerHTML = msg;
@@ -755,49 +840,17 @@ function confirm_no() {
     d.callback_no = null;
 }
 
-function eom_show(msg) {
-    show_accumulated_totals();
-    document.getElementById("eom-text").innerHTML = msg;
-    document.getElementById("eom-dialog").show();
-}
-
-function eom_close(ev) {
-    ev.preventDefault();
-    document.getElementById("eom-dialog").close();
-    document.getElementById("input-contract").focus();
-    clear_scorepad();
-    next_hand();
-}
-
-function edit_name_show(seat) {
-    document.getElementById("edit-name-input").value =
-        document.getElementById(seat + "-name").innerHTML;
-    let d = document.getElementById("edit-name-dialog");
-    d.seat = seat;
-    d.showModal();
-}
-
-function edit_name_finished(ev) {
-    if (ev.keyCode != 13)
-        return;
-    ev.preventDefault();
-    edit_name_close(ev);
-}
-
-function edit_name_close(ev) {
-    let d = document.getElementById("edit-name-dialog");
-    d.close();
-    let name = document.getElementById("edit-name-input").value || "";
-    document.getElementById(d.seat + "-name").innerHTML = name;
-    d.seat = null;
-}
+/*
+ * Initialization function
+ */
 
 window.onload = function() {
-    let names = document.getElementsByClassName("input-name");
-    for (let ne of names) {
-        ne.addEventListener("change", update_name);
-        update_name.call(ne);
-    }
+    /* Convert string with HTML entity to raw string */
+    let div = document.createElement("div");
+    div.innerHTML = NameConnector;
+    NameConnector = div.firstChild.nodeValue;
+
+    /* Add event listeners */
     for (let ne of document.getElementsByClassName("seat-name"))
         ne.addEventListener("click", edit_name);
     for (let de of document.getElementsByClassName("dealer"))
@@ -822,8 +875,10 @@ window.onload = function() {
         .addEventListener("keydown", edit_name_finished);
     document.getElementById("edit-name-close")
         .addEventListener("click", edit_name_close);
-    let se = document.getElementById("system");
-    se.addEventListener("change", change_system);
+    document.getElementById("system")
+        .addEventListener("change", change_system);
+
+    /* Finish setup */
+    set_system(document.getElementById("system").value);
     show_dealer(null);
-    set_system(se.value);
 }
